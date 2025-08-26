c300fc0cf99e1078481d8b41982a0fd424554194
dragon6810 <35243479+dragon6810@users.noreply.github.com>
2025-08-26 12:35:30 -0400

    can move pieces to hearts content

--- src/main.c
+++ src/main.c
@@ -9,8 +9,12 @@ board_t board = {};
 
 int main(int argc, char** argv)
 {
+    int i;
+
     char buf[MAX_INPUT];
-    char src[3], dst[3];
+    char movestr[2][3];
+    int r, f;
+    move_t move;
 
     board_loadfen(&board, "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
 
@@ -32,12 +36,31 @@ int main(int argc, char** argv)
             printf("        move a piece from source square to dest square, if legal.\n");
             printf("        src and dst are expected to be in algebraic notation.\n");
 
-            continue;;
+            continue;
         }
 
-        if(sscanf(buf, " move %2s %2s", src, dst) == 2)
+        if(sscanf(buf, " move %2s %2s", movestr[0], movestr[1]) == 2)
         {
-            printf("%s -> %s\n", src, dst);
+            for(i=0; i<2; i++)
+            {
+                if(movestr[i][0] < 'a' || movestr[i][0] > 'h')
+                    break;
+                if(movestr[i][1] < '1' || movestr[i][1] > '8')
+                    break;
+            }
+            if(i < 2)
+                continue;
+
+            for(i=0; i<2; i++)
+            {
+                r = movestr[i][1] - '1';
+                f = movestr[i][0] - 'a';
+                move[i] = r * BOARD_LEN + f;
+            }
+
+            board.pieces[move[1]] = board.pieces[move[0]];
+            board.pieces[move[0]] = 0;
+            board_print(&board);
 
             continue;
         }
