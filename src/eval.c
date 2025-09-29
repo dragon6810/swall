c2d8a00290ee3827f40186116e1c70633b3d8675
dragon6810 <35243479+dragon6810@users.noreply.github.com>
2025-09-29 13:44:29 -0400

    passed pawn bonus

--- src/eval.c
+++ src/eval.c
@@ -7,6 +7,65 @@ static const score_t startmaterial = 0
         + eval_pscore[PIECE_KNIGHT] * 2 
         + eval_pscore[PIECE_PAWN] * 8;
 
+static inline bitboard_t eval_passedpawnmask(team_e team, uint8_t square)
+{
+    const bitboard_t file = 0x1010101010101010;
+    const bitboard_t full = UINT64_MAX;
+
+    int8_t r, f;
+    bitboard_t mask;
+
+    r = square / BOARD_LEN;
+    f = square + BOARD_LEN;
+
+    mask = 0;
+    if(f)
+        mask |= file << (f - 1);
+    mask |= file << f;
+    if(f < BOARD_LEN - 1)
+        mask |= file << (f + 1);
+
+    if(team == TEAM_WHITE)
+        mask &= full << ((r + 1) * BOARD_LEN);
+    else
+        mask &= full >> ((BOARD_LEN - (r + 1)) * BOARD_LEN);
+
+    return mask;
+}
+
+static const score_t passedpawnbonus[BOARD_LEN] = { 0, 0, 10, 20, 40, 60, 80, 0 };
+
+static inline void eval_pawnstructure(board_t* board, score_t bonus[TEAM_COUNT])
+{
+    int i;
+    team_e t;
+
+    int8_t square, r;
+    bitboard_t mask;
+
+    for(t=0; t<TEAM_COUNT; t++)
+    {
+        bonus[t] = 0;
+        for(i=0; i<board->npiece[t]; i++)
+        {
+            square = board->ptable[t][i];
+            if((board->sqrs[square] & SQUARE_MASK_TYPE) != PIECE_PAWN)
+                continue;
+
+            r = square / BOARD_LEN;
+            mask = eval_passedpawnmask(t, square);
+
+            if(board->pboards[!t][PIECE_PAWN] & mask)
+                continue;
+
+            if(t == TEAM_BLACK)
+                r = BOARD_LEN - r;
+
+            bonus[t] += passedpawnbonus[r];
+        }
+    }
+}
+
 static inline void eval_positionbonus(board_t* board, float endgame, score_t bonus[TEAM_COUNT])
 {
     int i;
@@ -57,15 +116,16 @@ score_t evaluate(board_t* board)
 {
     team_e t;
 
-    score_t material[TEAM_COUNT], position[TEAM_COUNT], scores[TEAM_COUNT], eval;
+    score_t material[TEAM_COUNT], position[TEAM_COUNT], pawns[TEAM_COUNT], scores[TEAM_COUNT], eval;
     float endgameweight;
 
     eval_countmaterial(board, material);
     endgameweight = eval_endgameweight(material[TEAM_WHITE] + material[TEAM_BLACK]);
     eval_positionbonus(board, endgameweight, position);
+    eval_pawnstructure(board, pawns);
 
     for(t=0; t<TEAM_COUNT; t++)
-        scores[t] = material[t] + position[t];
+        scores[t] = material[t] + position[t] + pawns[t];;
 
     if(board->tomove == TEAM_WHITE)
         eval = scores[TEAM_WHITE] - scores[TEAM_BLACK];
